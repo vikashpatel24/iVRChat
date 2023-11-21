0b5383665b9d56f18ecdec8ce050a645d64d7226
its-vikash <dev.vikashpatel@gmail.com>
2023-11-21 07:46:51 +0530

    updated

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://127.0.0.1:5000";
+export const host = "https://ivrchat-backend.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
